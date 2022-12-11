2e39d316653781ceefb330e245f4e647040c0b8e
nurazom050 <nurazom048@gmail.com>
2022-12-11 21:36:58 +0600

    connect with mongos

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-
+//
+mongoose.connect("mongodb+srv://nuazomad:rr1234@cluster0.ybikdla.mongodb.net/?retryWrites=true&w=majority").then(function(){
 // home houte
-app.get("/", (req, res)=> {
-res.send("This is home");
+    app.get("/", (req, res)=> {
+        res.send("This is home connected mongoes");
+        
+        }),
+        
+        // notes  route
+        app.get("/notes", (req, res)=> {
+            res.send("This is notes mongoes note");
+            
+            });
+});
 
-}),
 
-// notes  route
-app.get("/notes", (req, res)=> {
-    res.send("This is notesf");
-    
-    }),
-app.listen(5000);
+
+app.listen(8080, function(){
+    console.log("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqserver started");
+});
