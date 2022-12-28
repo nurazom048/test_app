f39674227027e220a3c304a2bbf72eefffb04b34
nurazom050 <nurazom048@gmail.com>
2022-12-29 02:04:30 +0600

    comment

---
+++ controllers/comment_controllers.js
@@ -0,0 +1,65 @@
+const express = require("express");
+router = express.Router();
+const post =require("../models/post_models");
+const Video =require("../models/videos_model");
+const comment =require("../models/comment_model");
+
+require("mongoose");
+
+
+
+//........... add comment................./
+exports.add_comments =  async(req, res) => {
+     
+   comment_for_videos =  await  Video.findOne({_id :req.params.id });// to chack video is exsist or not
+    comment_for_post   =  await   post.findById( req.params.id);// to chack video is exsist or not
+    //console.log(comment_for_videos);
+  
+try {
+    const  comments = new comment({
+        id:req.params.id,
+        username:tokenowner.username,
+        comment: req.body.comment
+        });
+   
+if (comment_for_videos) {
+   
+    //..... add _comments
+        new_comment =   await comments.save()
+        console.log(new_comment) ;
+        
+
+
+    //..... add _comments id to videos comments[array]
+        updated_comment = await Video.findOneAndUpdate({ _id :req.params.id},{ $push : {comment: new_comment._id}},{new:true});
+        res.status(200).json({updated_comment } );
+        console.log("comment id added to videos");
+
+    
+} else if (comment_for_post){
+
+
+    //..... add _comments
+    new_comment =   await comments.save()
+    console.log(new_comment) ;
+    
+
+
+  //..... add _comments id to post comments[array]
+    updated_comment = await post.findOneAndUpdate({ _id :req.params.id},{ $push : {comment: new_comment._id}},{new:true});
+    res.status(200).json({updated_comment } );
+    console.log("comment id added to post");
+
+    
+}else if(!comment_for_post & !comment_for_videos){
+
+
+    res.status(400).json({error: "Please try again"});
+
+}else{
+}
+    
+} catch (error) {
+    throw(error);
+
+}}
