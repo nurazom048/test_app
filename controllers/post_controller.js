83ede28e9dabea5fc5eb2c9b1cb2ee01355d6a99
nurazom050 <nurazom048@gmail.com>
2023-01-02 22:04:57 +0600

    post imp

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -3,48 +3,53 @@ const { findOneAndDelete } = require("../models/post_models");
 router = express.Router();
 const post =require("../models/post_models");
 const Post =require("../models/post_models");
-const Video =require("../models/videos_model");
-
-
-const User = require('../models/account_model'); // import the Course model
-
 require("mongoose");
 
-/// upload post
-exports.upload_post = async(req, res) => {
 
-    console.log(req.user.username);
- 
-    const  uploadpost = new post({ username: req.user.username,...req.body  });
-try{
-    await uploadpost.save().then((u)=>{
-    console.log("post upload success") ;
-    res.status(200).json(u)});
-               
-} catch (error) {
-        throw(error)
-}   
 
-}
+
+
+
+///.............upload post
+exports.upload_post = async (req, res) => {
+    const { title, imgUrl } = req.body;
+    console.log({req: req.user});
+    try {
+     
+      const uploadPost = new Post({ 
+        userId: req.user._id,
+        username: req.user.username,
+        title,
+        imgUrl
+        
+      });
+      const savedPost = await uploadPost.save();
+      console.log('Post uploaded successfully');
+      return res.status(200).json(savedPost);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send({ error: 'Error uploading post' });
+    }
+  };
+  
+  
 
 //.............. edit_post.............
 exports.edit_post = async(req, res) => {
+    const {_id } = req.params
 
 try {
-    edited_post = await post.findOneAndUpdate({_id :req.params.pid },{ $set: req.body,},{new: true});
+    edited_post = await Post.findOneAndUpdate({_id :_id,userId:req.userId },{ $set: req.body,},{new: true});
  
 if ( edited_post) {
     console.log("post edited");
-    console.log(edited_post);
     res.status(200).json( edited_post);
-
-    
-} else {
+} 
+   if(!edited_post){
     console.log("you can edit only your own post");
     res.status(200).json({massage : "you can edit only your own post"});
+   }
 
-
-}
 
 } catch (error) {
     throw(error)   
@@ -52,33 +57,23 @@ if ( edited_post) {
 
 }
 
-///.............. delete_post.............
-exports.delete_post = async(req, res) => {
+//.............. delete_post.............
+exports.delete_post = async (req, res) => {
+    try {
+      const { _id } = req.params;
+      const deletedPost = await Post.findOneAndDelete({ _id, username: req.user.username });
+  
+      if (deletedPost) {
+        console.log("Post deleted");
+        return res.status(200).json({ message: "Deleted" });
+      }
+  
+      console.log("You can only delete your own post");
+      return res.status(200).json({ message: "You can only delete your own post" });
 
-find_post = await post.findOne({_id :req.params.pid});
-if (find_post) {
-//
-try {
 
-   deleted_post = await post.findOneAndDelete({_id :req.params.pid,username:req.user.username});
-    if (deleted_post) {
-        console.log("post deleted");
-        res.status(200).json({massage : "deleted"});
-    } else {
-        console.log("you can only delete your post ");
-        res.status(200).json({massage : "you can only delete your post "});
+    } catch (error) {
+      throw error;
     }
-   
-    
-} catch (error) {
-
-    throw(error)   
-
-}
-   
-} else {
-    console.log("post not found");
-    res.status(200).json({massage : "post not found"});
-
-}
-}
+  };
+  
